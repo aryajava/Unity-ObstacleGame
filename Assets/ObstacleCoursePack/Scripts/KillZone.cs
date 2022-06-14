bdf277dad5bf061a5d56202354b8e4aeda223a30
Muhammad Arya Java <muhammad.aryajava@gmail.com>
2022-06-14 16:49:55 +0700

    Upload Game

---
+++ Assets/ObstacleCoursePack/Scripts/KillZone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillZone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+			col.gameObject.GetComponent<CharacterControls>().LoadCheckPoint();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+}
